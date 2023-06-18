ffdbbfa65cbdf970f865788b759ef07607c3f311
dager <dager@goalem.solutions>
2023-06-18 20:56:03 +0300

    Add ssl verification

---
+++ ssl_verify/main.py
@@ -0,0 +1,12 @@
+from flask import send_file, Flask
+
+
+app = Flask('ssl')
+
+
+@app.route('.well-known/pki-validation/<file>')
+def verify(file):
+    return send_file(file)
+
+
+app.run(host='0.0.0.0', port=8080)
